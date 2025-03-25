e81f5757ee0dc6bcc16e0cf6c29c5f2f7a5ab681
Marcelosgc1 <marcelobtsgc1@gmail.com>
2025-03-24 21:20:31 -0300

    :sparkles: feat: MEF das operacoes

---
+++ cooprocessorFiles/simple_ula.v
@@ -0,0 +1,42 @@
+module simple_ula(
+	input clk,
+	input start,
+	input wire [3:0] opcode,
+	input wire [7:0] data_escalar,
+	input wire [199:0] matrizA,
+	input wire [199:0] matrizB,
+	output reg [199:0] matriz_resultante,
+	output reg done
+);
+	
+	wire [199:0] matriz_soma;
+
+	genvar i;
+ 
+	generate
+		for (i = 0; i<200; i = i + 8) begin : soma_matrizes
+			assign matriz_soma[i +: 8] = matrizA[i +: 8] + matrizB[i +: 8];
+		end
+	endgenerate
+
+
+	always @(posedge clk) begin
+		if (!start) begin
+			done <= 0;
+		end
+
+		if (start & !done) begin
+			case (opcode)
+				1: begin
+					matriz_resultante <= matriz_soma;
+					done <= 1;
+				end
+				
+				default: begin
+					done <= 1;
+				end
+			endcase
+		end
+	end
+
+endmodule
