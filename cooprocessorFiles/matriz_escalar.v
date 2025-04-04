18e919d82ad87d033576c9b1bdc032bcf8007ce3
David Neves Dias <148469360+davidm34@users.noreply.github.com>
2025-04-04 16:00:42 -0300

    Update matriz_escalar.v

--- cooprocessorFiles/matriz_escalar.v
+++ cooprocessorFiles/matriz_escalar.v
@@ -1,14 +1,12 @@
-module matriz_escalar(clk, data_escalar, matriz_a, matriz_b, matriz_resultante);
-	input clk;
+module matriz_escalar(data_escalar, matriz_a, matriz_resultante);
 	input [7:0] data_escalar;
 	input [199:0] matriz_a;
-	input [199:0] matriz_b;
 	output wire [199:0] matriz_resultante;
 
 	genvar i;
 	
 	generate 
-		for (i = 0; i < 25; i = i + 1) begin : subtracao_matrizes
+		for (i = 0; i < 25; i = i + 1) begin : escalar_matrizes
 				assign matriz_resultante[i*8 +: 8] = data_escalar * matriz_a[i*8+: 8];
 		end
 	endgenerate
