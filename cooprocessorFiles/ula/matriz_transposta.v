047ccc0d77c95877daace5e362b585a2a842ae80
Felipe Amorim <119305066+Felipeacs05@users.noreply.github.com>
2025-04-11 14:53:10 -0300

    Atualizar o matriz_transposta.v

--- cooprocessorFiles/ula/matriz_transposta.v
+++ cooprocessorFiles/ula/matriz_transposta.v
@@ -1,16 +1,16 @@
 module matriz_transposta(matrizA, matrizB, matriz_resultante);
-	input [199:0] matrizA;
-	input [199:0] matrizB;
-	output wire [199:0] matriz_resultante;
-	
-	genvar i,j;
-	
-	generate
-		for (j = 0; j < 5; j = j + 1) begin : linha
-			for (i = 0; i < 5; i = i + 1) begin : coluna
-				assign matriz_resultante[(8*i)+(40*j)+:8] = matrizA[(40*i)+(8*j) +:8];
-			end
-		end
-	endgenerate
+        input [199:0] matrizA; // Matriz original
+        input [199:0] matrizB; // Não utilizado aqui
+        output wire [199:0] matriz_resultante; // Matriz transposta
 
-endmodule 
+        genvar i,j;
+
+        generate
+                for (j = 0; j < 5; j = j + 1) begin : linha
+                        for (i = 0; i < 5; i = i + 1) begin : coluna
+                                assign matriz_resultante[(8*i)+(40*j)+:8] = matrizA[(40*i)+(8*j) +:8]; // Transpõe elementos
+                        end
+                end
+        endgenerate
+
+endmodule
