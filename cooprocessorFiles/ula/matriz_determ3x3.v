6e091e0ab7a8f8d01760fe2310d43dfa2269b401
Felipe Amorim <119305066+Felipeacs05@users.noreply.github.com>
2025-04-11 14:46:59 -0300

    Atualizar o matriz_determ3x3.v

--- cooprocessorFiles/ula/matriz_determ3x3.v
+++ cooprocessorFiles/ula/matriz_determ3x3.v
@@ -1,27 +1,27 @@
 module matriz_determ3x3(
-	 input wire [199:0] matriz_A,
+    input wire [199:0] matriz_A, // Matriz 5x5 em vetor linear
     input clk,
-    output wire [7:0] det
+    output wire [7:0] det // Saída do determinante 3x3
 );
 
-	wire [7:0] mat [4:0][4:0];
-	genvar i, j;
-	
-	generate begin
+    wire [7:0] mat [4:0][4:0]; // Matriz 5x5 com elementos de 8 bits
+    genvar i, j;
+
+    generate begin
         for (i = 0; i < 5; i = i + 1) begin : adad
             for (j = 0; j < 5; j = j + 1) begin : asdasd
-                assign mat[i][j] = matriz_A[(i * 40) + (j * 8) +: 8];
-			end end end
-	 endgenerate
-	 
-	 assign det = mat[0][0] * 
-	 (mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1]) 
-	 - mat[0][1] * (mat[1][0] * mat[2][2] - mat[1][2] * mat[2][0]) + mat[0][2] * 
-	 (mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0]);
+                assign mat[i][j] = matriz_A[(i * 40) + (j * 8) +: 8]; // Conversão do vetor para matriz
+            end
+        end
+    end endgenerate
 
-	// a b c
-	// d e f
-	// g h i
-	 
-	 
-endmodule 
+    assign det = mat[0][0] * 
+    (mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1]) 
+    - mat[0][1] * (mat[1][0] * mat[2][2] - mat[1][2] * mat[2][0]) + mat[0][2] * 
+    (mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0]); // Determinante 3x3
+
+    // a b c
+    // d e f
+    // g h i
+
+endmodule
