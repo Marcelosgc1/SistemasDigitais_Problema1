f1c24a62ee19bb1f56d85859f845b50cf43a2feb
Felipe Amorim <119305066+Felipeacs05@users.noreply.github.com>
2025-04-11 14:50:58 -0300

    Atualizar o matriz_oposta.v

--- cooprocessorFiles/ula/matriz_oposta.v
+++ cooprocessorFiles/ula/matriz_oposta.v
@@ -1,15 +1,12 @@
 module matriz_oposta(matrizA, matrizB, matriz_resultante);
-	input [199:0] matrizA;
-	input [199:0] matrizB;
-	output wire [199:0] matriz_resultante;
-	
-	
-	genvar i;
-	generate 
-		for (i = 0; i < 199; i = i + 8) begin : oposta_matriz
-			assign matriz_resultante[i +: 8] = -matrizA[i +: 8];
-		end
-	endgenerate 
-	
+        input [199:0] matrizA; // Matriz de entrada
+        input [199:0] matrizB; // Não utilizada
+        output wire [199:0] matriz_resultante; // Resultado da matriz oposta
 
-endmodule 
+        genvar i;
+        generate 
+                for (i = 0; i < 199; i = i + 8) begin : oposta_matriz
+                        assign matriz_resultante[i +: 8] = -matrizA[i +: 8]; // Inverte sinal de cada elemento
+                end
+        endgenerate 
+endmodule
