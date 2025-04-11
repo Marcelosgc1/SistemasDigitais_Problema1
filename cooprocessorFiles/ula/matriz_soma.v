40e6f2c61c756f2eac6f0ad59000de839d3d7264
Felipe Amorim <119305066+Felipeacs05@users.noreply.github.com>
2025-04-11 14:51:39 -0300

    Atualizar o matriz_soma.v

--- cooprocessorFiles/ula/matriz_soma.v
+++ cooprocessorFiles/ula/matriz_soma.v
@@ -1,17 +1,13 @@
 module matriz_soma(matrizA, matrizB, matriz_resultante);
-	input [199:0] matrizA;
-	input [199:0] matrizB;
-	output wire [199:0] matriz_resultante;
+	input [199:0] matrizA; // Primeira matriz de entrada
+	input [199:0] matrizB; // Segunda matriz de entrada
+	output wire [199:0] matriz_resultante; // Saída da soma das matrizes
 	
 	genvar i;
 	
 	generate
 		for (i = 0; i < 25; i = i + 1) begin : soma_matrizes
-			assign matriz_resultante[i*8 +: 8] = matrizA[i*8 +: 8] + matrizB[i*8 +: 8];
+			assign matriz_resultante[i*8 +: 8] = matrizA[i*8 +: 8] + matrizB[i*8 +: 8]; // Soma elemento a elemento
 		end
-		
 	endgenerate
-
-
-
-endmodule 
+endmodule
