a863102458a7b6192cae098cdf3aabd00a21ac8c
Felipe Amorim <119305066+Felipeacs05@users.noreply.github.com>
2025-04-11 14:45:05 -0300

    Atualizar o matriz_determ2x2.v

--- cooprocessorFiles/ula/matriz_determ2x2.v
+++ cooprocessorFiles/ula/matriz_determ2x2.v
@@ -1,24 +1,23 @@
 module matriz_determ2x2(
-	 input wire [199:0] matriz_A,
+    input wire [199:0] matriz_A, // Matriz 5x5 codificada como vetor linear
     input clk,
-    output wire [7:0] det
+    output wire [7:0] det // Determinante 2x2
 );
 
-	wire [7:0] mat [4:0][4:0];
-	genvar i, j;
-	
-	generate begin
+    wire [7:0] mat [4:0][4:0]; // Matriz 5x5 de elementos 8 bits
+    genvar i, j;
+
+    generate begin
         for (i = 0; i < 5; i = i + 1) begin : adad
             for (j = 0; j < 5; j = j + 1) begin : asdasd
-                assign mat[i][j] = matriz_A[(i * 40) + (j * 8) +: 8];
-			end end end
-	 endgenerate
-	 
-	 assign det = (mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]);
+                assign mat[i][j] = matriz_A[(i * 40) + (j * 8) +: 8]; // Mapeia o vetor para matriz
+            end
+        end
+    end endgenerate
 
-	// a b c
-	// d e f
-	// g h i
-	 
-	 
-endmodule 
+    assign det = (mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]); // Determinante 2x2
+
+    // a b
+    // c d
+
+endmodule
