598effe7246e87ed0adef8685710f112839ac9b3
Felipe Amorim <119305066+Felipeacs05@users.noreply.github.com>
2025-04-11 14:48:13 -0300

    Atualizar o matriz_determ4x4.v

--- cooprocessorFiles/ula/matriz_determ4x4.v
+++ cooprocessorFiles/ula/matriz_determ4x4.v
@@ -1,17 +1,17 @@
 module matriz_determ4x4 (
-    input wire [199:0] matriz_A,
+    input wire [199:0] matriz_A, // Matriz 5x5 em vetor
     input clk,
-	 input start,
-	 output reg done,
-    output reg [7:0] det
+    input start,
+    output reg done, // Indica fim do cálculo
+    output reg [7:0] det // Determinante 4x4
 );
 
-    wire [7:0] mat [4:0][4:0]; 
-    reg [31:0] tp [3:0];
+    wire [7:0] mat [4:0][4:0]; // Matriz 5x5
+    reg [31:0] tp [3:0]; // Termos temporários
     genvar i, j;
-    reg [4:0] count = 0;
+    reg [4:0] count = 0; // Contador
 
-    function [7:0] det_3x3;
+    function [7:0] det_3x3; // Função para determinante 3x3
         input [7:0] a, b, c, d, e, f, g, h, i;
         begin
             det_3x3 = a * (e * i - f * h) -
@@ -19,33 +19,33 @@ module matriz_determ4x4 (
                       c * (d * h - e * g);
         end
     endfunction
-	 
-	 generate begin
+
+    generate begin
         for (i = 0; i < 5; i = i + 1) begin : adad
             for (j = 0; j < 5; j = j + 1) begin : asdasd
-                assign mat[i][j] = matriz_A[(i * 40) + (j * 8) +: 8];
-			end end end
-	 endgenerate
+                assign mat[i][j] = matriz_A[(i * 40) + (j * 8) +: 8]; // Conversão vetor para matriz
+            end
+        end
+    end endgenerate
 
     always @(posedge clk) begin
-		  if(start) begin
-			  if (count < 4) begin
-					tp[count] = mat[0][count] * det_3x3(
-						 mat[1][(count+1)%4], mat[1][(count+2)%4], mat[1][(count+3)%4],
-						 mat[2][(count+1)%4], mat[2][(count+2)%4], mat[2][(count+3)%4],
-						 mat[3][(count+1)%4], mat[3][(count+2)%4], mat[3][(count+3)%4]
-					);
-			  end else begin
-					det = tp[0] - tp[1] + tp[2] - tp[3];
-					done = 1;
-			  end
-			  
-			  count <= count + 1;
-			  
-		  end else begin
-			done = 0;
-			det = 0;
-			count = 0;
-		  end
+        if(start) begin
+            if (count < 4) begin
+                tp[count] = mat[0][count] * det_3x3(
+                    mat[1][(count+1)%4], mat[1][(count+2)%4], mat[1][(count+3)%4],
+                    mat[2][(count+1)%4], mat[2][(count+2)%4], mat[2][(count+3)%4],
+                    mat[3][(count+1)%4], mat[3][(count+2)%4], mat[3][(count+3)%4]
+                ); // Expansão por cofatores
+            end else begin
+                det = tp[0] - tp[1] + tp[2] - tp[3]; // Soma dos cofatores
+                done = 1;
+            end
+
+            count <= count + 1;
+        end else begin
+            done = 0;
+            det = 0;
+            count = 0;
+        end
     end
 endmodule
