0923cf886acad25bf3997a3dc3d9acc40d66588a
Marcelosgc1 <marcelobtsgc1@gmail.com>
2025-05-24 00:26:36 -0300

    :sparkles: feat: better convolution

--- cooprocessorFiles/ula/matriz_multi.v
+++ cooprocessorFiles/ula/matriz_multi.v
@@ -5,29 +5,41 @@ module matriz_multi (
   input signed [199:0] matriz_b, // Matriz B (5x5, 8 bits por elemento)
   input clock,
   input wire start,
-  output [399:0] result, // Resultado da multiplicação
+  output [199:0] result, // Resultado da multiplicação
   output reg done // Sinal de conclusão
 );
 	
 	wire [400:0] matriz_resultante;
+	wire [199:0] convolution, modulo, to_positive;
+	
 
+	
+	always @ (posedge clock) begin
+		done <= 1;
+	end
 	
 	
 //	convolution algorithm:
 //	
 //	escalar multi
 // soma results
+// conv pronto!
 //
-//	
+// verifica se eh negativo
+//
+//
+//
+
+
     genvar i;
 
     generate
         for (i = 0; i < 25; i = i + 1) begin : escalar_matrizes
-            assign matriz_resultante[i*16 +: 16] = matriz_b[i*8+: 8] * matriz_a[i*8+: 8]; // Multiplica cada elemento pelo escalar
+            assign matriz_resultante[i*16 +: 16] = matriz_b[i*8+: 8] * {1'b0, matriz_a[i*8+: 8]}; // Multiplica cada elemento pelo escalar
         end
     endgenerate
 
-	assign result =
+	assign convolution =
       $signed(matriz_resultante[0*16 +: 16])  + $signed(matriz_resultante[1*16 +: 16]) +
       $signed(matriz_resultante[2*16 +: 16])  + $signed(matriz_resultante[3*16 +: 16]) +
       $signed(matriz_resultante[4*16 +: 16])  + $signed(matriz_resultante[5*16 +: 16]) +
@@ -41,5 +53,38 @@ module matriz_multi (
       $signed(matriz_resultante[20*16 +: 16]) + $signed(matriz_resultante[21*16 +: 16]) +
       $signed(matriz_resultante[22*16 +: 16]) + $signed(matriz_resultante[23*16 +: 16]) +
       $signed(matriz_resultante[24*16 +: 16]);
+		
+		
+		
+
+	assign to_positive = ~convolution + 1'b1;
+		
+	assign modulo = convolution[199] ? to_positive : convolution;
+		
+		
+		
+		
+
+		
+		
+	
+	assign overflow = |(modulo[199:8]);
+	//nor(overflow, t0, t1);
+	
+	assign result = {overflow, modulo[7:0]};
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	 
 endmodule
