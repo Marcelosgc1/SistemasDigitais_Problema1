0189d4abdfcceba8edfbb8c1fb3974383095d7b1
Marcelosgc1 <marcelobtsgc1@gmail.com>
2025-05-27 20:42:39 -0300

    :sparkles: feat: return all results

--- cooprocessorFiles/ula/matriz_multi.v
+++ cooprocessorFiles/ula/matriz_multi.v
@@ -1,23 +1,24 @@
 module matriz_multi (
   input signed [199:0] matriz_a, // Matriz A (5x5, 8 bits por elemento)
   input signed [199:0] matriz_b, // Matriz B (5x5, 8 bits por elemento)
-  input [1:0] seletor,
-  input clock,
+  input seletor,
+  input clk,
   input wire start,
-  output [7:0] result, // Resultado da multiplicação
+  output [199:0] result, // Resultado da multiplicação
   output reg done // Sinal de conclusão
 );
 
 
-parameter zero = 8'b00000000;
+parameter ZERO = 8'b00000000;
 
 wire [199:0] matriz_c, matriz_d, aux_kernel;
-wire [19:0] r1, r2, absSum;
+wire [19:0]  r1, r2;
+wire [7:0]   absSum;
 
 matriz_transposta(matriz_b,, matriz_c);
-assign matriz_d = {matriz_b[8+:8],matriz_b[48+:8],zero,zero,zero,matriz_b[0+:8],matriz_b[40+:8]};
+assign matriz_d = {matriz_b[8+:8],matriz_b[48+:8],ZERO,ZERO,ZERO,matriz_b[0+:8],matriz_b[40+:8]};
 
-assign aux_kernel = seletor[0] ? matriz_d : matriz_c;
+assign aux_kernel = seletor ? matriz_d : matriz_c;
 
 matriz_conv uni_mtr(matriz_a, matriz_b, clk, start, r1, done1);
 
@@ -31,7 +32,6 @@ assign overflow = |(tempSum[20:8]);
 
 assign absSum = overflow ? 200'hff : tempSum[7:0];
 
-assign result = seletor[1] ? absSum : r1;
 
 //SELETOR:
 //
@@ -44,24 +44,31 @@ reg state = 0;
 reg [20:0]tempSum;
 
 
-always @ (posedge clock) begin
+always @ (posedge clk) begin
 
 	if (!start) begin
 		done <= 0;
 		state <= 0;
 	end
 	else begin
-		if(done1 & done2) begin
-			tempSum <= r1+r2;
-			state <= 1;
-		end
-		if(state) begin
-			done <= 1;
-		end
+		case (state) 
+			0: begin
+				if(done1 & done2) begin
+					tempSum <= r1+r2;
+					state <= 1;
+				end
+			end
+			1: begin
+				if(state) begin
+					done <= 1;
+				end
+			end
+		endcase
 	end
-
 end
 
+
+assign result = {ZERO,absSum,r2[7:0],r1[7:0]};
 
 
 
